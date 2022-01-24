36e72cecfb6b161f45a8627bc1325a574659338f
Rob Bos <rajbos@users.noreply.github.com>
2022-01-24 21:17:03 +0100

    Move script to file

---
+++ .github/workflows/src/load-users.js
@@ -0,0 +1,21 @@
+
+odule.exports = ({github, context, owner, repo, userFile}) => {
+        
+    console.log(`repo = ${repo}`)
+    try {
+    const yml = await github.rest.repos.getContent({
+        owner: owner,
+        repo: repo,
+        path: userFile
+    })
+    console.log(`repo = ${repo}`)
+    console.log(`yml result = ${yml}`)
+    console.log(`end`)
+    
+    } catch (error) {
+    console.log(`No ${userFile} file found in repository: ${repo}: ${error}`)
+    }
+
+    //const parsed = YAML.parse(content)
+
+}
