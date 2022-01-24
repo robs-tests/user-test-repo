1b86440c8b6d4a32727369500d26e61f84e8943a
Rob Bos <rajbos@users.noreply.github.com>
2022-01-24 22:52:47 +0100

    searching

--- src/load-users.js
+++ src/load-users.js
@@ -19,7 +19,6 @@ async function run() {
         throw error
     }
 
-    console.log(JSON.stringify(content))
     const parsed = yaml.parse(content)
     console.log(`Found ${parsed.users.length} users`)
     for (let num = 0; num < parsed.users.length; num++) {
@@ -27,12 +26,50 @@ async function run() {
         console.log(`Found user: [${element}]`)
 
         // handle the user
+        handleUser()
 
     }  
 
     return 1
 }
 
+function handleUser (userHandle){
+    console.log(`Handling user ${userHandle}`)
+    // test if it actually is a proper user handle
+    userUrl = `https://api.github.com/users/${userHandle}`
+    let result
+    try {
+       result = await github.request({url: userUrl}) 
+    } catch (error) {
+      console.log(`Error retrieving user with handle ${userHandle}: ${error}`)  
+    }   
+
+    if (!result) {
+        return
+    }
+
+    const collaboratorsUrl = `https://api.github.com/${owner}//${repo}/collaborators`
+    result = await github.request({url: collaboratorsUrl})
+    if (!result) {
+        return
+    }
+
+    // find if user already is a collaborator on this repo
+    console.log(`log: ${JSON.stringify(result)}`)
+
+    const isFound = result.some(element => {
+        if (element.login === userHandle) {
+          return true;
+        }
+      });
+
+    if (isFound) {
+        console.log(`User ${userHandle} already is a collaborator on repo ${owner}/${repo}`)
+    }
+    else {
+        console.log(`Adding user ${userHandle} to repo ${owner}/${repo}`)
+    }
+}
   
   console.log(`1`)
   return await run()
