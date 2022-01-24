55d3ac14ac76b45d162a457c4c369a55a3958bd1
Rob Bos <rajbos@users.noreply.github.com>
2022-01-24 21:36:53 +0100

    test

--- src/load-users.js
+++ src/load-users.js
@@ -3,22 +3,21 @@ module.exports = async ({github, context, owner, repo, userFile, yaml}) => {
         
     console.log(`repo = ${repo}`)
     try {
-    const yml = await github.rest.repos.getContent({
-        owner: owner,
-        repo: repo,
-        path: userFile
-    })
-    console.log(`repo = ${repo}`)
-    const parsed = yaml.parse(yml)
-    console.log(`yml result = ${parsed}`)
-    console.log(`end`)
+        const yml = await github.rest.repos.getContent({
+            owner: owner,
+            repo: repo,
+            path: userFile
+        })
+        console.log(`repo = ${repo}`)
+        const parsed = yaml.parse(yml)
+        console.log(`yml result = ${parsed}`)
+        console.log(`end`)
+
+        for (const element of parsed.users) {
+            console.log(`Found user: [${element}]`)
+        }  
     
     } catch (error) {
         console.log(`No ${userFile} file found in repository: ${repo}: ${error}`)
     }
-
-    for (const element of parsed.users) {
-        console.log(`Found user: [${element}]`)
-    }  
-
 }
