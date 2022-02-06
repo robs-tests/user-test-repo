6deb369c01f1a572d53a9046fa20f87b7a64a314
Rob Bos <raj.bos@gmail.com>
2022-02-06 16:54:19 +0100

    Added sending an org invite

--- src/load-users.js
+++ src/load-users.js
@@ -36,37 +36,51 @@ async function run() {
 async function handleUser (userHandle, organization){
     console.log(`Handling user ${userHandle} for organization ${organization}`)
     // test if it actually is a proper user handle
-    userUrl = `https://api.github.com/users/${userHandle}`
-    let result
+    const userUrl = `https://api.github.com/users/${userHandle}`
+    let user
     try {
-       result = await github.request({url: userUrl}) 
+       user = await github.request({url: userUrl}) 
        console.log(`Handle exists`)
     } catch (error) {
       console.log(`Error retrieving user with handle ${userHandle}: ${error}`)  
     }   
 
-    if (!result) {
+    if (!user) {
         return
     }
 
-    // todo: add PAT with access to get this info /orgs/{org}/members/{username}
+    // find if user is already member on this org
     const membersUrl = `https://api.github.com/orgs/${organization}/members/${userHandle}`
-    result = (await github.request({url: membersUrl}))
-    if (!result || result.length === 0) {
+    user = (await github.request({url: membersUrl}))
+    if (!user || user.length === 0) {
         console.log(`Members is empty`)
     }
     
-    // find if user already is a collaborator on this repo
-    console.log(`log: ${JSON.stringify(result)}`)
-
-    let isFound = result.status == 204
+    let isFound = user.status == 204
     if (isFound) {
         console.log(`User ${userHandle} already is a member on this organization ${organization}`)
     }
     else {
         console.log(`Adding user ${userHandle} to organization ${organization}`)
 
-        addUserToOrganization(userHandle, organization)
+        addUserToOrganization(user.id, organization)
+    }
+}
+
+function addUserToOrganization(userId, organization) {
+    console.log(`Adding the user ${userId} to the organization ${organization}`)
+
+    // todo: test for open invites before sending new ones
+        
+    // POST /orgs/{org}/invitations
+    const url = "POST /orgs/${organization}/invitations"
+
+    try {
+        github.request(url, {
+            invitee_id:userId
+        })
+    } catch (error) {
+        console.log(`Error sending invite for userId ${userId}: ${error}`)
     }
 }
   
